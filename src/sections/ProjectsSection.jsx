2e9f1c2bef43e8d93ed0599305146dc1d945a915
Daniel Molina <danielparada1985@gmail.com>
2024-08-08 01:10:11 -0400

    final version, responsive design added.

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -8,26 +8,28 @@ export default function ProjectsSection() {
   const [sel, setSel] = useState(0);
 
   function handleChange(index) {
-    const dataContainer = document.querySelector(".project-data");
-    const imageContainer = document.querySelector(".image-container");
+    if (index !== sel) {
+      const dataContainer = document.querySelector(".project-data");
+      const imageContainer = document.querySelector(".image-container");
 
-    dataContainer.classList.add("is-change");
-    imageContainer.classList.add("is-change");
-
-    setTimeout(() => {
-      setSel(index);
-      dataContainer.classList.remove("is-change");
+      dataContainer.classList.add("is-change");
+      imageContainer.classList.add("is-change");
 
       setTimeout(() => {
-        imageContainer.classList.remove("is-change");
-      }, 300);
-    }, 200);
+        setSel(index);
+        dataContainer.classList.remove("is-change");
+
+        setTimeout(() => {
+          imageContainer.classList.remove("is-change");
+        }, 300);
+      }, 200);
+    }
   }
 
   useEffect(() => {
     const observer = createIntersectionObserver({
       threshold: 0.9,
-      rootMargin: "0px",
+      rootMargin: "0px 100px",
     });
 
     observer.observe(document.querySelector(".projects-container"));
